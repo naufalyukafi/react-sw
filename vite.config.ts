9e043416e18aa815bcd4d5739b27deee5597a9fe
naufalyukafi <naufalyukafi10@gmail.com>
2024-01-28 13:36:07 +0700

    feat: simple caching strategies using react vite pwa

---
+++ vite.config.ts
@@ -0,0 +1,29 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { VitePWA, } from 'vite-plugin-pwa'
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    VitePWA({
+      workbox: {
+        globPatterns: ["**/*.{js,ts,tsx,css,html,ico,png,svg}"],
+        cacheId: 'cache-v1',
+        skipWaiting: true,
+        clientsClaim: true,
+        runtimeCaching: [
+          {
+            urlPattern: /\.(?:png|jpg|jpeg|svg)$/,
+            handler: 'CacheFirst',
+            options: {
+              expiration: {
+                maxEntries: 10,
+              },
+              cacheName: 'images',
+            }
+          },
+        ],
+      }
+    })
+  ],
+})
